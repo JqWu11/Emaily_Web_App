43fc7fc303a247a881fb96c39a78a011026ff272
Justin Wu <jqwu@umich.edu>
2024-05-26 22:39:02 -0400

    changed message

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send({hi: 'there'});
+    res.send({Justin: 'Wu'});
 });
 //heroku looks for the underlying environment to see if there is a port to use
 const PORT = process.env.PORT || 5000;
