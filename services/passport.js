8ac90316aeab48e087701a87b39035c02b35c9d2
Justin Wu <jqwu@umich.edu>
2024-05-29 13:10:10 -0400

    added MongoDB

--- services/passport.js
+++ services/passport.js
@@ -5,6 +5,17 @@ const keys = require('../config/keys');
 
 const user = mongoose.model('users');
 
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+    user.findById(id)
+    .then(user => {
+        done(null,user);
+    });
+});
+
 passport.use(
     new GoogleStrategy({
     clientID: keys.googleClientID,
@@ -12,15 +23,15 @@ passport.use(
     callbackURL: '/auth/google/callback'
     }, 
     (accessToken, refreshToken, profile, done) => {
-        user.findOne({googleID: profile.id})
-        .then((existingUser) => {
-            if(exisingUser){
+        user.findOne({googleID: profile.id}).then((existingUser) => {
+            if (existingUser){
                 //already have a record with the profile id
+                done(null, existingUser);
             }
             else{
-                new user ({googleID: profile.id}).save();
-
+                new user ({googleID: profile.id}).save()
+                .then(user => done(null, user));
             }
-        })
+        });
 })
 );
