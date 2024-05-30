173ebad85816ae8ec364818f4fe26f8c156e6b49
Justin Wu <jqwu@umich.edu>
2024-05-29 22:49:47 -0400

    dev vs prod environment

--- index.js
+++ index.js
@@ -27,3 +27,7 @@ app.listen(PORT, () => {
     console.log(`Our app is running on port ${ PORT }`);
 });
 
+//connection string: mongodb+srv://jqwu11:X5GHW5FY01Es0ouW@cluster0.gijloyd.mongodb.net/emailyprod?retryWrites=true&w=majority&appName=Cluster0
+//client secret: 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
+//client id: 189093211742-58untkhtcbqqheko0q27iks7ksvllmrf.apps.googleusercontent.com
+//arbitrary string: iuhfeqwiuhfeoqifehwi
