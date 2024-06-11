189c33a0efdec5e7f3730a1171060a81c7251572
Justin Wu <jqwu@umich.edu>
2024-06-11 17:54:58 -0400

    version control

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,13 +1,16 @@
 import { FETCH_USER } from '../actions/types';
+
 const initialState = {}; // or any default state you prefer
 
-export default function(state = initialState, action){
+// Naming the function here
+function authReducer(state = initialState, action) {
     console.log(action);
-    switch (action.type){
+    switch (action.type) {
         case FETCH_USER:
             return action.payload || false;
-
         default:
             return state;
     }
 }
+
+export default authReducer;
