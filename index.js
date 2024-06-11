58a83db97f72c98ee3608a82c4f6102033238280
Justin Wu <jqwu@umich.edu>
2024-06-11 14:21:43 -0400

    billing and client

--- index.js
+++ index.js
@@ -27,6 +27,16 @@ app.use(passport.session());
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
 
+if(process.env.NODE_ENV === 'production'){
+    //express will serve up produciton assets for some file
+    app.use(express.static('client/build'));
+    //Express serves up the index.html file it the
+    //route is not recognied
+    const path = require('path');
+    app.get('*',(req,res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    })
+}
 
 //heroku looks for the underlying environment to see if there is a port to use
 const PORT = process.env.PORT || 5000;
