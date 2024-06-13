d4adca3ae84a19f492e181a13deb5c216bca7c79
Justin Wu <jqwu@umich.edu>
2024-06-12 21:24:55 -0400

    users change

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cookieSession = require('cookie-session');
 const passport = require('passport');
 const keys = require('./config/keys');
 const bodyParser = require('body-parser');
-require('./models/User');
+require('./models/users');
 require('./models/Survey');
 require('./services/passport');
 
