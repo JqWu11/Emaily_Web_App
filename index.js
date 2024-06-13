73dc35a2166e49b2089275ee273218789f005ae1
Justin Wu <jqwu@umich.edu>
2024-06-12 21:11:36 -0400

    dev change

--- index.js
+++ index.js
@@ -2,47 +2,39 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
-const bodyParser = require('body-parser');
 const keys = require('./config/keys');
-require('./models/users');
+const bodyParser = require('body-parser');
+require('./models/User');
 require('./models/Survey');
 require('./services/passport');
 
-
-mongoose.connect(keys.mongoURI);
-
+mongoose.connect(keys.mongoURI, { useMongoClient: true });
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(
-cookieSession({
-maxAge: 30 * 24 * 60 * 60 * 10000,
-keys: [keys.cookieKey]
-})
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        keys: [keys.cookieKey]
+    })
 );
+
 app.use(passport.initialize());
 app.use(passport.session());
 
-
+// ROUTES
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
 require('./routes/surveyRoutes')(app);
 
-if(process.env.NODE_ENV === 'production'){
-    //express will serve up produciton assets for some file
+if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
-    //Express serves up the index.html file it the
-    //route is not recognied
     const path = require('path');
-    app.get('*',(req,res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    })
+ app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+ });
 }
 
-//heroku looks for the underlying environment to see if there is a port to use
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-console.log(`Our app is running on port ${ PORT }`);
-});
-
+const PORT = process.env.PORT || 5000
+app.listen(PORT);
