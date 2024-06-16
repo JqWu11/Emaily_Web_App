71c5afb87fb03628bf3fa84ebb845f7dac93aea9
Justin Wu <jqwu@umich.edu>
2024-06-16 12:52:02 -0400

    name change

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -31,7 +31,7 @@ class Header extends Component {
         <div className="nav-wrapper">
           <Link to={this.props.auth ? "/surveys" : "/"}
           className="left brand-logo">
-            Emaily
+            SurveyMaster
           </Link>
           <ul className="right">
             {this.renderContent()}
