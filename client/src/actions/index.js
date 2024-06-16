c590395ca478bd5e99634d0176741e2cde43b1d0
Justin Wu <jqwu@umich.edu>
2024-06-16 12:35:37 -0400

    d

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -13,7 +13,7 @@ export const handleToken = (token) => async dispatch => {
    dispatch({ type:FETCH_USER, payload: res.data});
 };
 
-export const submitSurvey = (values, history) => async dispach => {
+export const submitSurvey = (values, history) => async dispatch => {
    const res = await axios.post('/api/surveys', values);
 
    history.push('/surveys');
